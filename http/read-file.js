0ebb81c70a7fa860fa94cb65bb19a3b7aead6e9d
kouh <kouh28@gmail.com>
2015-06-30 15:16:38 +0900

    ファイル読み込みは非同期化

--- http/read-file.js
+++ http/read-file.js
@@ -4,9 +4,17 @@ var
 
 
 http.createServer(function(req, res){
-  var fileContents = fs.readFileSync(__dirname + '/public/hello.txt');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(fileContents);
+  fs.readFile(__dirname + '/public/hello.txt', 'utf-8', function (err, data){
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end(fileContents);
+      return;
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(data);
+    
+  });
 }).listen(3000);
 
 console.log('Server running at localhost:3000');
